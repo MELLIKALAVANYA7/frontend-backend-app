5d316867f8e056df6220a0602debbc4d5625e06f
Lavanya <mellikalavanya7@gmail.com>
2025-12-04 17:39:40 +0530

    Fix API URL

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,18 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
-});
-
-// automatically attach token
-api.interceptors.request.use((config) => {
-  if (typeof window !== "undefined") {
-    const token = localStorage.getItem("token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-  }
-  return config;
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
 export default api;
